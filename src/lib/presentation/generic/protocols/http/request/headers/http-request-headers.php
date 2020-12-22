07b960dca6264bb2807de38ff48bb67b31512f52
Arthur <arthur.software.developer@gmail.com>
2020-12-22 18:02:29 -0300

    feat: initial HttpRequestHeadersInterface structure

--- src/lib/presentation/generic/protocols/http/request/headers/http-request-headers.php
+++ src/lib/presentation/generic/protocols/http/request/headers/http-request-headers.php
@@ -2,6 +2,23 @@
 
 namespace lib\presentation\generic\protocols\http\request\headers;
 
-abstract class HttpRequestHeadersInterface {}
+use lib\data\protocols\headers\cycle\CycleHeadersInterface;
+
+abstract class HttpRequestHeadersInterface {
+  /** @var CycleHeadersInterface $instance headers controller instance */
+  public CycleHeadersInterface $instance;
+  /** @var string $accept resquest accept */
+  public string $accept;
+  /** @var string $content_type resquest content type */
+  public string $content_type;
+  /** @var string $host resquest method */
+  public string $host;
+  /** @var string $origin resquest origin */
+  public string $origin;
+  /** @var string $referer resquest referer */
+  public string $referer;
+  /** @var string $user_agent resquest user agent */
+  public string $user_agent;
+}
 
 ?>
