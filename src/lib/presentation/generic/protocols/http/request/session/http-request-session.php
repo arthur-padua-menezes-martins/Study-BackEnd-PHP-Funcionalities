36e246eaa20b7a1da0223caa46e930828de55c88
Arthur <arthur.software.developer@gmail.com>
2020-12-22 18:02:38 -0300

    feat: initial HttpRequestSessionInterface structure

--- src/lib/presentation/generic/protocols/http/request/session/http-request-session.php
+++ src/lib/presentation/generic/protocols/http/request/session/http-request-session.php
@@ -2,6 +2,11 @@
 
 namespace lib\presentation\generic\protocols\http\request\session;
 
-abstract class HttpRequestSessionInterface {}
+use lib\data\protocols\session\cycle\CycleSessionInterface;
+
+abstract class HttpRequestSessionInterface {
+  /** @var CycleSessionInterface $instance session controller instance */
+  public CycleSessionInterface $instance;
+}
 
 ?>
