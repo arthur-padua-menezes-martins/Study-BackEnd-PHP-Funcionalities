b3198ffb0b0207659c594387616f9c647a252926
Arthur <arthur.software.developer@gmail.com>
2020-12-22 18:02:48 -0300

    feat: initial HttpRequestOptionsInterface structure

---
+++ src/lib/presentation/generic/protocols/http/request/options/http-request-options.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace lib\presentation\generic\protocols\http\request\options;
+
+abstract class HttpRequestOptionsInterface {
+  /** @var string $method resquest method */
+  public string $method;
+}
+
+?>
