5079b9f34114e90c405b37a497aecb12c34a352b
Arthur <arthur.software.developer@gmail.com>
2020-12-22 17:54:23 -0300

    feat: initial HttpRequestBodyInterface structure

--- src/lib/presentation/generic/protocols/http/request/body/http-request-body.php
+++ src/lib/presentation/generic/protocols/http/request/body/http-request-body.php
@@ -2,6 +2,34 @@
 
 namespace lib\presentation\generic\protocols\http\request\body;
 
-abstract class HttpRequestBodyInterface {}
+abstract class HttpRequestBodyInterface {
+  private static array $body = [];
+
+  /**
+  * get body property
+  * @param string $property property name
+  * @return mixed property value
+  */
+  public function __get(string $property) {
+    if (isset($this->body[$property])) {
+      $body_property = clone $this->body[$property];
+    } else {
+      $body_property = null;
+    }
+
+    return $body_property;
+  }
+
+  /**
+  * set body property
+  * @param string $property property name
+  * @param mixed $value property value
+  */
+  public function __set(string $property, $value): void {
+    if (isset($value)) {
+      $this->body[$property] = $value;
+    }
+  }
+}
 
 ?>
