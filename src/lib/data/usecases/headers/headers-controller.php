6d05e1c89738474166aae53b38af5a6c4f125e71
Arthur <arthur.software.developer@gmail.com>
2020-12-24 17:39:59 -0300

    chore: delete files

--- src/lib/data/usecases/headers/headers-controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace lib\data\usecases\headers;
-
-use lib\data\usecases\headers\cycle\CycleHeaders;
-
-abstract class HeadersController extends CycleHeaders {}
-
-?>
--- src/lib/data/usecases/session/session-controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace lib\data\usecases\session;
-
-use lib\data\usecases\session\cycle\CycleSession;
-
-abstract class SessionController extends CycleSession {}
-
-?>
