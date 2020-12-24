50c9400895ffa1e4a686c67936f24890354bfd97
Arthur <arthur.software.developer@gmail.com>
2020-12-24 18:12:13 -0300

    refactor: code refactor in headers life cycle

---
+++ src/lib/data/usecases/http/headers/htpp-life-cycle-headers.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace lib\data\usecases\http\headers;
+
+use lib\data\protocols\http\headers\LifeCycleHeadersInterface;
+
+/** auxiliary to headers life cycle */
+final class LifeCycleHeaders implements LifeCycleHeadersInterface {
+  /** restricted constructor */
+  private function __construct() {}
+
+  /**
+  * start headers
+  * @return self class instace
+  */
+  static public function start(): self {
+    $headers_instance = new self(); 
+    return $headers_instance;
+  }
+
+  /**
+  * set headers
+  * @param array $headers headers content
+  */
+  static public function set_headers(array $headers): void {
+    foreach ($headers as $header => $value ) {
+      if ($value) {
+        header("{$header}: {$value}");
+      } else {
+        header("{$header}");
+      }
+    }
+  }
+
+  /**
+  * set status code
+  * @param int $statusCode status code content
+  */
+  static public function set_status_code(int $statusCode): void {
+    http_response_code($statusCode);
+  }
+}
+
+?>
