715facfb1f6cc58387c459020c394a56ec551a39
Arthur <arthur.software.developer@gmail.com>
2020-12-24 18:14:12 -0300

    feat: initial url life cycle structur

---
+++ src/lib/data/protocols/http/url/http-life-cycle-url-interface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace lib\data\protocols\http\url;
+
+/** url life cycle */
+interface LifeCycleUrlInterface {}
+
+?>
---
+++ src/lib/data/usecases/http/url/htpp-life-cycle-url.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace lib\data\usecases\http\url;
+
+use lib\presentation\generic\protocols\http\request\options\HttpRequestOptionsInterface;
+
+use lib\data\protocols\http\url\LifeCycleUrlInterface;
+
+/** auxiliary to url life cycle */
+final class LifeCycleUrl implements LifeCycleUrlInterface {
+  private HttpRequestOptionsInterface $requestOptions;
+
+  public function __construct(HttpRequestOptionsInterface $requestOptions) {
+    $this->requestOptions = $requestOptions;
+  }
+}
+
+?>
