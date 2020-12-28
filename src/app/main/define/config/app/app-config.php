bf16066cba6806ff3053d754b4ba7dc8dc90c2f3
Arthur <arthur.software.developer@gmail.com>
2020-12-28 13:49:16 -0300

    feat: initial app config

---
+++ src/app/main/define/config/app/app-config.php
@@ -0,0 +1,11 @@
+<?php
+
+/** application base url */
+define('APP_BASE_URL', 'localhost');
+/** development base url */
+define('DEV_BASE_URL', 'localhost');
+
+/** application port */
+define('APP_PORT', 8080);
+
+?>
