889be40c8a57cea4a29b59ab900e7e23a9184393
Arthur <arthur.software.developer@gmail.com>
2020-12-27 23:28:26 -0300

    feat: create HttpRequestEnum

---
+++ src/lib/presentation/generic/protocols/http/request/http-request-enum.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace lib\presentation\generic\protocols\http\request;
+
+/** application request type */
+class HttpRequestEnum {
+  /** @var string request headers */
+  const headers = 'headers';
+  /** @var string request session */
+  const session = 'session';
+  /** @var string request body */
+  const body = 'body';
+  /** @var string request params */
+  const params = 'params';
+  /** @var string request query */
+  const query = 'query';
+  /** @var string request options */
+  const options = 'options';
+}
+
+?>
